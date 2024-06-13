72f82c23b1331f879df8238a7e3fa08a7c6bb9b5
Timothy Carll, MD <146137122+TimCarll@users.noreply.github.com>
2024-06-13 13:15:38 -0500

    Delete .github/update.bat

--- .github/update.bat
+++
@@ -1,5 +0,0 @@
-REM cd "N:\Coag\Lupus\Lupus Reports\6. Archived Auto Drafts\python\LACReport"
-REM "N:\Coag\Lupus\Lupus Reports\6. Archived Auto Drafts\python\Git\bin\git.exe" pull
-REM copy /Y *.py ..
-REM cd "N:\Coag\Lupus\Lupus Reports\Tools\python"
-
