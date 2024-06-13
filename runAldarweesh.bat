25236f1d9cc86cb2ca653d9cffebd8eb96ef6bae
Timothy Carll, MD <146137122+TimCarll@users.noreply.github.com>
2024-06-13 13:02:54 -0500

    Add files via upload

---
+++ runAldarweesh.bat
@@ -0,0 +1,5 @@
+cd "N:\Coag\Lupus\Lupus Reports\Tools\python"
+call update.bat
+python.exe runAldarweesh.py
+
+copy /Y "N:\Coag\Lupus\Lupus Reports\3. Need Pathologist Review\*.docx" "N:\Coag\Lupus\Lupus Reports\6. Archived Auto Drafts\"
