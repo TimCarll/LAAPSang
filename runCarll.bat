fb8aa9f936e9ddf5bf8cd0cc0b8eb53e28d44e7f
Timothy Carll, MD <146137122+TimCarll@users.noreply.github.com>
2024-07-05 15:23:22 -0500

    Delete runCarll.bat

--- runCarll.bat
+++
@@ -1,5 +0,0 @@
-cd "N:\Coag\Lupus\Lupus Reports\Tools\python"
-call update.bat
-python.exe runCarll.py carll
-
-copy /Y "N:\Coag\Lupus\Lupus Reports\3. Need Pathologist Review\*.docx" "N:\Coag\Lupus\Lupus Reports\6. Archived Auto Drafts\"
